0889095f4bc768b25d539ad1989d7b34d0eb49ca
Jason Atallah <jasonatallah@gmail.com>
2023-01-19 16:38:38 -0800

    added custom color to tailwind config for bg to match design.png
    added more custom colors to tailwind config
    
    more custom colors, finished design of new search

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
-import CityWeather from "../components/city-weather";
+import CityWeather from "../components/city-weather-refactor";
 
-export default function IndexPage() {
+function OldIndexPage() {
   const [city, setCity] = useState<string | null>(null);
+
   return (
     <div className="py-2">
       <form
@@ -25,11 +26,43 @@ export default function IndexPage() {
         </button>
       </form>
 
-      {city && (
-        <div className="mt-4">
-          <CityWeather city={city} />
+      <div className="mt-4">
+        <CityWeather
+          city={"Rohnert Park"}
+          temperature={40}
+          description={"clear sky"}
+          iconCode="10d"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default function IndexPage() {
+  const [city, setCity] = useState("");
+
+  return (
+    <div className="flex flex-col items-center gap-12 p-8">
+      <form className="flex gap-2">
+        <span className="text-lg self-center">Weather Search: </span>
+        <div className="flex">
+          <input
+            type="text"
+            id="city"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-l-xl focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+          <button className="bg-blue-450 text-white p-3 rounded-r-xl">
+            SUBMIT
+          </button>
         </div>
-      )}
+      </form>
+      <CityWeather
+        city={"Rohnert Park"}
+        temperature={40}
+        description={"clear sky"}
+        iconCode="10d"
+      />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        blue: {
+          450: "#4683C8",
+        },
+        gray: {
+          650: "#4C5566",
+        },
+        neutral: {
+          450: "#A3AEBE",
+        },
+        slate: {
+          150: "#E3E8EF",
+        },
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
