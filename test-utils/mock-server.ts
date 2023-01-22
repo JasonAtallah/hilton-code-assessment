14ceee46fb96f8444b643e41cc4d4f094608137e
Jason Atallah <jasonatallah@gmail.com>
2023-01-22 14:27:10 -0800

    updated mock server

--- test-utils/mock-server.ts
+++ test-utils/mock-server.ts
@@ -1,9 +1,13 @@
-import { rest } from "msw";
+import { rest, setupWorker } from "msw";
 import { setupServer } from "msw/node";
 import { MOCK_RESULTS, TEMPERATURE_IN_KELVIN } from "./mock-results";
 
-export const server = setupServer(
-  rest.get("https://api.openweathermap.org/*", (req, res, ctx) => {
+const IS_BROWSER = typeof window === undefined;
+
+// Add any other http request mock handlers into this array
+const handlers = [
+  rest.get(`${process.env.OPEN_WEATHER_BASE_URL}/*`, (req, res, ctx) => {
+    console.log("is this running????");
     return res(
       ctx.json({
         weather: [
@@ -15,5 +19,11 @@ export const server = setupServer(
         main: { temp: TEMPERATURE_IN_KELVIN },
       })
     );
-  })
-);
+  }),
+];
+
+export const getServer = () => setupServer(...handlers);
+export const getWorker = () => setupWorker(...handlers);
+
+export const setupMocks = async () =>
+  IS_BROWSER ? getWorker().start() : getServer().listen();
